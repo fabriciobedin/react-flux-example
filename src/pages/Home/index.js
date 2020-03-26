36f13013c1d101666d22d4b78f91dd56024862c2
Fabricio Bedin <fabricioobedin@gmail.com>
2020-03-25 23:51:10 -0300

    adding json-server and working with api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,95 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
+import api from '../../services/api';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="http://static.netshoes.com.br/produtos/tenis-under-armour-charged-carbon-masculino/93/B78-4580-793/B78-4580-793_zoom1.jpg"
-          alt="Product"
-        />
-        <strong> The best product </strong>
-        <span> $79,90</span>
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="fff" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { products: [] };
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      formattedPrice: formatPrice(product.price)
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.formattedPrice}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="fff" /> 3
+              </div>
+              <span>Add to cart</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
