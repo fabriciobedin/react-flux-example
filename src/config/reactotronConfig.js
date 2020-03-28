a4bfc7cb3cc4a68fc3a5b3665afb3af65667b89b
Fabricio Bedin <fabricioobedin@gmail.com>
2020-03-28 20:06:57 -0300

    adding reactotron to debug

---
+++ src/config/reactotronConfig.js
@@ -0,0 +1,9 @@
+import reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = reactotron.configure().use(reactotronRedux()).connect();
+
+  tron.clear();
+  console.tron = tron;
+}
